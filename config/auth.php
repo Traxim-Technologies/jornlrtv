00cbf2c66b72d4e259cdc448856bd2cf4ea17c1c
Shobana Chandrasekar <shobana@streamhash.com>
2017-03-22 12:56:21 +0530

    Fixed forgo password page view issue

--- config/auth.php
+++ config/auth.php
@@ -113,7 +113,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'email' => 'auth.emails.password',
+            'email' => 'user.auth.emails.password',
             'table' => 'password_resets',
             'expire' => 60,
         ],
