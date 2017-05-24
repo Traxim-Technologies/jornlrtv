798d4b2c9ca22953374601a36cd11e4248396777
Shobana-Chandrasekar <shobana@streamhash.com>
2017-05-24 12:38:33 +0530

    Compress Job

--- app/Repositories/CommonRepository.php
+++ app/Repositories/CommonRepository.php
@@ -382,7 +382,7 @@ class CommonRepository {
 
                     if (file_exists($inputFile)) {
                         Log::info("Main queue Videos : ".'Success');
-                        dispatch(new CompressVideo($inputFile, $local_url, MAIN_VIDEO, $model->id, $file_name));
+                        dispatch(new CompressVideo($inputFile, $local_url, $model->id, $file_name));
                         Log::info("Main queue completed : ".'Success');
                     }
                 }
