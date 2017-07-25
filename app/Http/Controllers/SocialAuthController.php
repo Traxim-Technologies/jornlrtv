2ef01eb4d68ce5dba11c14edf040ae176e8ba886
aravinthramesh2 <aravinth+1@provenlogic.net>
2017-07-25 15:42:07 +0530

    chanegs

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -18,52 +18,80 @@ class SocialAuthController extends Controller
         return Socialite::driver($request->provider)->redirect();
     }
 
-    public function callback(Request $request ,$provider)
-	{
+    public function callback(Request $request ,$provider) {
 
 		$social_user = \Socialite::driver($provider)->user();
 
-		$user = User::where('social_unique_id' , $social_user->id)->where('login_by' , $provider)->first();
+		$user = new User;
 
-		if(!count($user)) {
+		$check_user = [];
 
-			$user = new User;
-			$user->social_unique_id = $social_user->id;
-			$user->login_by = $provider;
+		$user->email = "social".uniqid()."@streamhash.com";
 
-			if($social_user->name) {
-				$user->name = $social_user->name;
-			} else {
-				$user->name = "Dummy";
-			}
+		// Check the social login has email 
+
+		if($social_user->email) {
+
+			// Check the record exists
+
+			$check_user = User::where('email',$social_user->email)->first();
+
+		} else {
+			
+			// Check social unique ID Already exists
+			
+			// $user = User::where('social_unique_id' , $social_user->id)->where('login_by' , $provider)->first();
+
+			$check_user = User::where('social_unique_id' , $social_user->id)->first()
 
 			if($social_user->email && !User::where('email',$social_user->email)->first()) {
+
 				$user->email = $social_user->email;
-			} else {
-				$user->email = "social".uniqid()."@streamhash.com";
+
 			}
-
-			// Save Dummy details
-			$user->picture =  asset('placeholder.png');
-
-			if(in_array($provider, array('facebook','twitter'))) {
-				if($social_user->avatar_original) {
-					$user->picture = $social_user->avatar_original;
-				}
-			}
-
-			$user->password = Hash::make($social_user->id);
-
-            $user->token = Helper::generate_token();
-            $user->token_expiry = Helper::generate_token_expiry();
-
-            $user->is_verified = 1;
-
-			$user->save();
-
 		}
 
-	    auth()->login($user);
+		if($check_user) {
+
+			$user = $check_user;
+		}
+
+		$user->social_unique_id = $social_user->id;
+
+		$user->login_by = $provider;
+
+		if($social_user->name) {
+			$user->name = $social_user->name;
+		} else {
+			$user->name = "Dummy";
+		}
+
+		// Save Dummy details
+
+		$user->picture =  asset('placeholder.png');
+
+		if(in_array($provider, array('facebook','twitter'))) {
+
+			if($social_user->avatar_original) {
+				$user->picture = $social_user->avatar_original;
+			}
+			
+		}
+
+		$user->password = Hash::make($social_user->id);
+
+        $user->token = Helper::generate_token();
+        $user->token_expiry = Helper::generate_token_expiry();
+
+        $user->is_verified = 1;
+
+		$user->save();
+
+
+		if($user) {
+	    	auth()->login($user);
+		}
+
 
 	    return redirect()->route('user.dashboard');
 	}
