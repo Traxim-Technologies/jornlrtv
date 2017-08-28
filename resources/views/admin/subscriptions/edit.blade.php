0b6131446220721bfec9d5e6a1dbc5001de65b24
Shobana-Chandrasekar <shobana@streamhash.com>
2017-08-28 16:04:49 +0530

    Issues fixed

--- resources/views/admin/subscriptions/edit.blade.php
+++ resources/views/admin/subscriptions/edit.blade.php
@@ -68,7 +68,7 @@
                             <label for="amount" class="">{{tr('amount')}}</label>
 
                             <!-- <div class="col-sm-10"> -->
-                                <input type="text" value="{{isset($data) ? $data->amount : old('amount')}}" name="amount" class="form-control" id="amount" placeholder="amount" pattern="[0-9]{1,}">
+                                <input type="text" value="{{isset($data) ? round($data->amount) : old('amount')}}" name="amount" class="form-control" id="amount" placeholder="amount" pattern="[0-9]{1,}">
                             <!-- </div> -->
                         </div>
 
