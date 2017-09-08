dd91c9dd757315bdab6737c4e46ef4955f835266
Shobana-Chandrasekar <shobana@streamhash.com>
2017-09-08 15:21:33 +0530

    Issues fixed

--- resources/views/admin/payments/video-payments.blade.php
+++ resources/views/admin/payments/video-payments.blade.php
@@ -28,7 +28,7 @@
 						      <th>{{tr('username')}}</th>
 						      <th>{{tr('payment_id')}}</th>
 						      <th>{{tr('amount')}}</th>
-						      <th>{{tr('expiry_date')}}</th>
+						      <th>{{tr('paid_date')}}</th>
 						    </tr>
 						</thead>
 
@@ -42,7 +42,7 @@
 							      	<td><a target="_blank" href="{{route('admin.view.user' , $payment->user_id)}}"> {{$payment->user->name}} </a></td>
 							      	<td>{{$payment->payment_id}}</td>
 							      	<td>$ {{$payment->amount}}</td>
-							      	<td>{{date('d M Y',strtotime($payment->expiry_date))}}</td>
+							      	<td>{{$payment->created_at->diffForHumans()}}</td>
 							    </tr>					
 
 							@endforeach
