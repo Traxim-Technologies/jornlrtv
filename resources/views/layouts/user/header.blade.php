2ef241510725a382765ea3b401c78e0cc635a102
Shobana-Chandrasekar <shobana@streamhash.com>
2017-10-19 19:54:51 +0530

    Header Issues fixed

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -67,6 +67,12 @@
                           </ul>
                         </div>
                 </div><!--y-button end-->
+
+            @else
+                <div class="y-button" style="position: unset">
+                    <a href="{{route('user.login.form')}}" class="y-signin">{{tr('login')}}</a>
+                </div><!--y-button end-->
+
             @endif
 
             <ul class="nav navbar-nav pull-left">
@@ -103,72 +109,72 @@
 
         <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
 
-        <div id="custom-search-input" class="">
-            <form method="post" action="{{route('search-all')}}" id="userSearch">
-            <div class="input-group">
-                
-                    <input type="text" id="auto_complete_search" name="key" class="search-query form-control" required placeholder="Search" />
-                    <span class="input-group-btn">
-                        <button class="btn btn-danger" type="submit">
-                        <span class=" glyphicon glyphicon-search"></span>
-                        </button>
-                    </span>
-                
-            </div>
-            </form>
-        </div><!--custom-search-input end-->
+            <div id="custom-search-input" class="">
+                <form method="post" action="{{route('search-all')}}" id="userSearch">
+                <div class="input-group">
+                    
+                        <input type="text" id="auto_complete_search" name="key" class="search-query form-control" required placeholder="Search" />
+                        <span class="input-group-btn">
+                            <button class="btn btn-danger" type="submit">
+                            <span class=" glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+                    
+                </div>
+                </form>
+            </div><!--custom-search-input end-->
 
         </div>
 
         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 hidden-xs visible-sm visible-md visible-lg">
-        @if(Auth::check())
-            <div class="y-button profile-button">
-               <div class="dropdown">
-                      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                        @if(Auth::user()->picture != "")
-                            <img class="profile-image" src="{{Auth::user()->picture}}">
-                        @else
-                            <img class="profile-image" src="{{asset('placeholder.png')}}">
-                        @endif
-                        
-                      </button>
-                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <li><a href="{{route('user.profile')}}">{{tr('profile')}}</a></li>
-                        @if(Setting::get('payment_type') == 'stripe')
-                            <li><a href="{{route('user.card.card_details')}}">{{tr('cards')}}</a></li>
-                        @endif
-                        <li><a href="{{route('user.channels.subscribed')}}">{{tr('subscribed_channels')}}</a></li>
-                        <li><a href="{{route('user.wishlist')}}">{{tr('wishlist')}}</a></li>
-                        <li><a href="{{route('user.history')}}">{{tr('history')}}</a></li>
-                        @if(Setting::get('is_spam')) 
-                        <li><a href="{{route('user.spam-videos')}}">{{tr('spam_videos')}}</a></li>
-                        @endif
+            @if(Auth::check())
+                <div class="y-button profile-button">
+                   <div class="dropdown">
+                          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            @if(Auth::user()->picture != "")
+                                <img class="profile-image" src="{{Auth::user()->picture}}">
+                            @else
+                                <img class="profile-image" src="{{asset('placeholder.png')}}">
+                            @endif
+                            
+                          </button>
+                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="{{route('user.profile')}}">{{tr('profile')}}</a></li>
+                            @if(Setting::get('payment_type') == 'stripe')
+                                <li><a href="{{route('user.card.card_details')}}">{{tr('cards')}}</a></li>
+                            @endif
+                            <li><a href="{{route('user.channels.subscribed')}}">{{tr('subscribed_channels')}}</a></li>
+                            <li><a href="{{route('user.wishlist')}}">{{tr('wishlist')}}</a></li>
+                            <li><a href="{{route('user.history')}}">{{tr('history')}}</a></li>
+                            @if(Setting::get('is_spam')) 
+                            <li><a href="{{route('user.spam-videos')}}">{{tr('spam_videos')}}</a></li>
+                            @endif
 
-                        @if(Setting::get('is_payper_view')) 
-                         <li><a href="{{route('user.pay-per-videos')}}">{{tr('pay_per_videos')}}</a></li>
+                            @if(Setting::get('is_payper_view')) 
+                             <li><a href="{{route('user.pay-per-videos')}}">{{tr('pay_per_videos')}}</a></li>
 
-                         @endif
-                        @if (Auth::user()->login_by == 'manual') 
+                             @endif
+                            @if (Auth::user()->login_by == 'manual') 
+                                <li role="separator" class="divider"></li>
+                                <li><a href="{{route('user.change.password')}}">{{tr('change_password')}}</a></li>
+                            @endif
+
                             <li role="separator" class="divider"></li>
-                            <li><a href="{{route('user.change.password')}}">{{tr('change_password')}}</a></li>
-                        @endif
+                            <li><a href="{{route('user.delete.account')}}" @if(Auth::user()->login_by != 'manual') onclick="return confirm('Are you sure? . Once you deleted account, you will lose your history and wishlist details.')" @endif>{{tr('delete_account')}}</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="{{route('user.logout')}}">{{tr('logout')}}</a></li>
+                          </ul>
+                        </div>
+                </div><!--y-button end-->
+            @else
+                <div class="y-button">
+                    <a href="{{route('user.login.form')}}" class="y-signin">{{tr('login')}}</a>
+                </div><!--y-button end-->
 
-                        <li role="separator" class="divider"></li>
-                        <li><a href="{{route('user.delete.account')}}" @if(Auth::user()->login_by != 'manual') onclick="return confirm('Are you sure? . Once you deleted account, you will lose your history and wishlist details.')" @endif>{{tr('delete_account')}}</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="{{route('user.logout')}}">{{tr('logout')}}</a></li>
-                      </ul>
-                    </div>
-            </div><!--y-button end-->
-        @else
-            <div class="y-button">
-                <a href="{{route('user.login.form')}}" class="y-signin">{{tr('login')}}</a>
-            </div><!--y-button end-->
-
-        @endif
+            @endif
 
 
-        <ul class="nav navbar-nav pull-right">
+            <ul class="nav navbar-nav pull-right">
 
                 @if(Setting::get('admin_language_control'))
 
