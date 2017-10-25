0851f6fcc9d1f46119d33274cd9aad6bd3e0f715
Shobana-Chandrasekar <shobana@streamhash.com>
2017-10-25 18:11:58 +0530

    UserSeeder Token changed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,25 +17,6 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->delete();
 
-        DB::table('users')->insert([
-            [
-                'name' => 'Admin',
-                'email' => 'admin@tubenow.com',
-                'password' => \Hash::make('123456'),
-                'picture' =>"https://tubenow.streamhash.com/placeholder.png",
-                'chat_picture' =>"https://tubenow.streamhash.com/placeholder.png",
-                'token'=>AppJwt::create(['id' => 1, 'email' => 'admin@tubenow.com', 'role' => "model"]),
-                'token_expiry'=>Helper::generate_token_expiry(),
-                'dob'=>'1992-01-01',
-                'age_limit'=>25,
-                'is_verified'=>1,
-                'status'=>1,
-                'user_type'=>1,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ],
-        ]);
-
     	DB::table('users')->insert([
     		[
 		        'name' => 'User',
@@ -43,7 +24,7 @@ class UserSeeder extends Seeder
 		        'password' => \Hash::make('123456'),
 		        'picture' =>"https://tubenow.streamhash.com/placeholder.png",
                 'chat_picture' =>"https://tubenow.streamhash.com/placeholder.png",
-                'token'=>AppJwt::create(['id' => 2, 'email' => 'user@tubenow.com', 'role' => "model"]),
+                'token'=>AppJwt::create(['id' => 1, 'email' => 'user@tubenow.com', 'role' => "model"]),
                 'token_expiry'=>Helper::generate_token_expiry(),
                 'dob'=>'1992-01-01',
                 'age_limit'=>25,
