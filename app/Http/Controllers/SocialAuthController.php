741b756f11e949a88deca943f7bc5950bd68d293
aravinthramesh2 <aravinth+1@provenlogic.net>
2017-11-18 15:36:24 +0530

    social auth fixed

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -36,83 +36,99 @@ class SocialAuthController extends Controller
 
 		}
 
-		$social_user = \Socialite::driver($provider)->user();
-
-		$user = new User;
-
-		$check_user = [];
-
-		$user->email = "social".uniqid()."@streamhash.com";
-
-		// Check the social login has email 
-
-		if($social_user->email) {
-
-			// Check the record exists
-
-			$check_user = User::where('email',$social_user->email)->first();
-
-			if(!$check_user) {
-				$user->email = $social_user->email;
-			}
+		if($provider == "twitter") {
+			
+			$social_user = \Socialite::driver($provider)->user();
+			
 
 		} else {
+
+			$social_user = \Socialite::driver($provider)->stateless()->user();
+
+		}
+
+		if(count($social_user) > 0)
+
+			$user = new User;
+
+			$check_user = [];
+
+			$user->email = "social".uniqid()."@streamhash.com";
+
+			// Check the social login has email 
+
+			if($social_user->email) {
+
+				// Check the record exists
+
+				$check_user = User::where('email',$social_user->email)->first();
+
+				if(!$check_user) {
+					$user->email = $social_user->email;
+				}
+
+			} else {
+				
+				// Check social unique ID Already exists
+				
+				// $user = User::where('social_unique_id' , $social_user->id)->where('login_by' , $provider)->first();
+
+				$check_user = User::where('social_unique_id' , $social_user->id)->first();
+
+				if($social_user->email && !User::where('email',$social_user->email)->first()) {
+
+					$user->email = $social_user->email;
+
+				}
 			
-			// Check social unique ID Already exists
-			
-			// $user = User::where('social_unique_id' , $social_user->id)->where('login_by' , $provider)->first();
+			}
 
-			$check_user = User::where('social_unique_id' , $social_user->id)->first();
+			if($check_user) {
 
-			if($social_user->email && !User::where('email',$social_user->email)->first()) {
+				$user = $check_user;
+			}
 
-				$user->email = $social_user->email;
+			$user->social_unique_id = $social_user->id;
+
+			$user->login_by = $provider;
+
+			if($social_user->name) {
+				$user->name = $social_user->name;
+			} else {
+				$user->name = "Dummy";
+			}
+
+			// Save Dummy details
+
+			$user->picture =  asset('placeholder.png');
+
+			if(in_array($provider, array('facebook','twitter'))) {
+
+				if($social_user->avatar_original) {
+					$user->picture = $social_user->avatar_original;
+				}
 
 			}
-		}
 
-		if($check_user) {
+			$user->password = Hash::make($social_user->id);
 
-			$user = $check_user;
-		}
+	        $user->token = Helper::generate_token();
+	        
+	        $user->token_expiry = Helper::generate_token_expiry();
 
-		$user->social_unique_id = $social_user->id;
+	        $user->is_verified = 1;
 
-		$user->login_by = $provider;
+			$user->save();
 
-		if($social_user->name) {
-			$user->name = $social_user->name;
-		} else {
-			$user->name = "Dummy";
-		}
 
-		// Save Dummy details
-
-		$user->picture =  asset('placeholder.png');
-
-		if(in_array($provider, array('facebook','twitter'))) {
-
-			if($social_user->avatar_original) {
-				$user->picture = $social_user->avatar_original;
+			if($user) {
+		    	auth()->login($user);
 			}
 
-		}
+	    	return redirect()->route('user.dashboard');
 
-		$user->password = Hash::make($social_user->id);
-
-        $user->token = Helper::generate_token();
-        $user->token_expiry = Helper::generate_token_expiry();
-
-        $user->is_verified = 1;
-
-		$user->save();
-
-
-		if($user) {
-	    	auth()->login($user);
-		}
-
-
-	    return redirect()->route('user.dashboard');
+	    } else {
+	    	return back()->with('flash_error' , tr('something_error'));
+	    }
 	}
 }
