6d0039de162ecec0f231094a111e8c598e5d73a7
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-01-02 16:27:16 +0530

    push chganges

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -618,9 +618,9 @@
 
                     Log::info('Check users variable');
 
-                    $data = ['video_tape_id' => 1];
+                    $data = ['video_tape_id' => 1 , 'channel_id' => 109 ];
 
-                    $push_message = ['success' => true,'title' => $message,'type' => 1,'data' => $data];
+                    $push_message = ['success' => true,'title' => $message,'type' => 2,'data' => $data];
 
                     foreach ($users as $key => $user) {
 
