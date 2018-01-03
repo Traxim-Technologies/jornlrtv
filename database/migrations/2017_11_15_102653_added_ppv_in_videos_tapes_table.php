08ebf4e6e0509f3c69200f7c6b16d85b27a8e619
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-01-03 22:19:17 +0530

    migration changes

--- database/migrations/2017_11_15_102653_added_ppv_in_videos_tapes_table.php
+++ database/migrations/2017_11_15_102653_added_ppv_in_videos_tapes_table.php
@@ -13,8 +13,8 @@ class AddedPpvInVideosTapesTable extends Migration
     public function up()
     {
         Schema::table('video_tapes', function (Blueprint $table) {
-            // $table->integer('type_of_user')->default(0)->after('user_ratings');
-            // $table->integer('type_of_subscription')->default(0)->after('type_of_user');
+            $table->integer('type_of_user')->default(0)->after('user_ratings');
+            $table->integer('type_of_subscription')->default(0)->after('type_of_user');
             $table->float('ppv_amount')->default(0)->after('type_of_subscription');
             $table->float('admin_ppv_amount')->default(0)->after('ppv_amount');
             $table->float('user_ppv_amount')->default(0)->after('admin_ppv_amount');
