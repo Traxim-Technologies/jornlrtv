09d00ac49f4fcb8036c251be880f2e72084d8b40
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-03-10 01:58:02 +0530

    issue fixes

--- resources/views/admin/reviews/reviews.blade.php
+++ resources/views/admin/reviews/reviews.blade.php
@@ -51,7 +51,11 @@
 							      	<td>
 							      		<a href="{{route('admin.view.video' , array('id' => $review->video_id))}}" target="_blank">{{$review->title}}</a>
 							      	</td>
-							      	<td><input id="view_rating" name="rating" type="number" class="rating view_rating" min="1" max="5" step="1" value="{{$review->rating}}"></td>
+							      	<td>
+							      		<span style="display: none;">{{$review->rating}}</span>
+							      		<input id="view_rating" name="rating" type="number" class="rating view_rating" min="1" max="5" step="1" value="{{$review->rating}}">
+
+							      	</td>
 							      	<td>{{$review->comment}}</td>
 							      	<td>{{$review->created_at->diffForHumans()}}</td>
 							      	<td><a href="{{route('admin.delete_user_ratings', array('id'=>$review->rating_id))}}" title="{{tr('delete')}}" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a></td>
