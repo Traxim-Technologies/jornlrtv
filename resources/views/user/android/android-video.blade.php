dfdca16b2b20a0b85843d04fde54360371a1a7a7
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-04-20 19:44:22 +0530

    video size issue fixed

--- resources/views/user/android/android-video.blade.php
+++ resources/views/user/android/android-video.blade.php
@@ -61,18 +61,18 @@ console.log(appSettings);
 
 <body>
 
-<div class="slide-area recom-area abt-sec" ng-app="liveApp" >
+<div class="slide-area recom-area abt-sec" ng-app="liveApp" style="position: relative;">
 
-	<div class="row live_video">
+	<div class="live_video">
 
 		<div class="col-lg-8" ng-controller="androidCtrl" ng-cloak ng-init="initRoom({{$data->id}}, '{{$data->virtual_id}}')">
 
 			<div class="live_img" id="videos-container" room="{{$data->id}}" style="position: fixed;height: 100%;
-			background-color: #000;width: 100%;top:0;left:0;">
+			background-color: #000;width: 100%;top:0;left:0;justify-content: center;display: flex;align-items: center;">
 				<!-- <img src="{{asset('images/mobile-camera.jpg')}}" width="100%" height="400px"> -->
 
 				<!-- <img src="{{asset('images/preview_img.jpg')}}" width="100%" id="default_image"> -->
-				<div style="background-image: url({{asset('images/mobile-camera.jpg')}});" style="background-size: cover;background-position: center;background-repeat: no-repeat;height: 100%;"></div>
+				<div style="background-image: url({{asset('images/mobile-camera.jpg')}});background-size: cover;background-position: center;background-repeat: no-repeat;height: 100%;"></div>
 				
 
 				<div class="loader_img" id="loader_btn" style="display: none;"><img src="{{asset('images/loader.svg')}}"/></div>
