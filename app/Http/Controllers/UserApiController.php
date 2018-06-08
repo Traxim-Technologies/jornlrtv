0a58e2b59df0b2a6c641c6e182ecd511f9e28c03
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-06-08 23:04:17 +0530

    send push notification issue fixed

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -223,7 +223,7 @@ class UserApiController extends Controller {
             
             $error_messages = implode(',',$validator->messages()->all());
            
-            $response_array = array('success' => false, 'error' => 'Invalid Input', 'error_code' => 101, 'error_messages' => $error_messages );
+            $response_array = array('success' => false, 'error' => tr('invalid_input'), 'error_code' => 101, 'error_messages' => $error_messages );
        
         } else {
 
