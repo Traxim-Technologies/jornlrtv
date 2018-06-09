4e37e7e705fcf7f5157c3eeac933c923fae185c6
Your Name <you@example.com>
2018-06-09 16:11:33 +0530

    Streamtube-v1.2 - Add seeder
    Summary:
        Added payment type seeder in settings table
    
    Test Plan:
    	* Payment type seeder
    	* Fixed channel list trying to property object error
    
    Reviewers: Vithya, Shobana, Aravinth
    
    Differential Revision: http://review.botfingers.com/D91

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -36,5 +36,6 @@ class DatabaseSeeder extends Seeder
         $this->call(SecureVideoSeeder::class);
         $this->call(MailGunSeeder::class);
         $this->call(IosPaymentStatusSeeder::class);
+        $this->call(PaymentTypeSeeder::class);
     }
 }
