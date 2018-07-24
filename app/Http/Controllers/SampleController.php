9808b4dc0dd4e0337f4f0d438127dc1afea6d917
aravinthramesh2 <aravinth+1@provenlogic.net>
2018-07-24 19:50:08 +0530

    sample url added for angeli cient

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SampleController extends Controller
+{
+    public function angelo_in_app_purchase(Request $request) {
+
+    	\Log::info(print_r($reqeust->all() , true));
+
+    	return response()->json(['success' => true]);
+    }
+}
