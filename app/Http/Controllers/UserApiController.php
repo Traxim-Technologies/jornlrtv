b6d23d53d8448581ac3f646db8b351ccaa2c10cb
Shobana Chandrsekar <shobana@codegama.com>
2018-09-07 16:08:41 +0530

    Search list - issues fixed

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -1676,11 +1676,13 @@ class UserApiController extends Controller {
 
             $base_query = VideoTape::where('video_tapes.is_approved' , 1)   
                                 ->videoResponse()
-                                ->leftJoin('channels' , 'video_tapes.channel_id' , '=' , 'channels.id')     
+                                ->leftJoin('channels' , 'video_tapes.channel_id' , '=' , 'channels.id')
+                                ->leftJoin('categories' , 'categories.id' , '=' , 'video_tapes.category_id')
                                 ->where('video_tapes.status' , 1)
                                 ->where('video_tapes.publish_status' , 1)
                                 ->where('channels.is_approved', 1)
                                 ->where('channels.status', 1)
+                                ->where('categories.status', CATEGORY_APPROVE_STATUS)
                                 ->where('title', 'like', "%".$request->key."%")
                                 ->orderby('video_tapes.watch_count' , 'desc');
                                //  ->select('video_tapes.id as video_tape_id' , 'video_tapes.title');
