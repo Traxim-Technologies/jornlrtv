3dde2feed188729b5dc9763854e366d69d2e7bdf
vithyaCodegama <vithya@codegama.com>
2019-03-26 14:11:02 +0530

    [v4.0] - User panel changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1153,11 +1153,11 @@ Route::group(['as' => 'user.'], function(){
 
     // Notifications
 
-    Route::any('bell_notifications/', 'UserController@bell_notifications')->name('bell_notifications.index');
+    Route::any('notifications/', 'UserController@bell_notifications')->name('bell_notifications.index');
 
-    Route::any('bell_notifications/update', 'UserController@bell_notifications_update')->name('bell_notifications.update');
+    Route::any('notifications/update', 'UserController@bell_notifications_update')->name('bell_notifications.update');
 
-    Route::any('bell_notifications/count', 'UserController@bell_notifications_count')->name('bell_notifications.count');
+    Route::any('notifications/count', 'UserController@bell_notifications_count')->name('bell_notifications.count');
 
     // User Playlists
 
