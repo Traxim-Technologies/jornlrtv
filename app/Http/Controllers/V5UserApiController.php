f788e4b5afb2b4d7de8c2f44954ef4dc0f0e75db
vithyaCodegama <vithya@codegama.com>
2019-05-07 13:48:10 +0530

    [v5.0] - Api change

--- app/Http/Controllers/V5UserApiController.php
+++ app/Http/Controllers/V5UserApiController.php
@@ -25,7 +25,7 @@ class V5UserApiController extends Controller
 
 	public function __construct(Request $request) {
 
-        $this->middleware('UserApiVal', ['except' => ['channels_index', 'channel_view', 'channel_based_videos']]);
+        $this->middleware('UserApiVal', ['except' => ['channels_index', 'channels_view', 'channel_based_videos']]);
 
         $this->middleware('ChannelOwner' , ['only' => ['video_tapes_status', 'video_tapes_delete', 'video_tapes_ppv_status','video_tapes_publish_status']]);
 
