2e83d439903a5f8a2de7299db2e49b6c0e26c260
vithyaCodegama <vithya@codegama.com>
2019-06-26 12:26:51 +0530

    iOS - API Issue fixes

--- app/Redeem.php
+++ app/Redeem.php
@@ -6,12 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Redeem extends Model
 {
-// 	public function getTotalAttribute($value)
-//     {
-// // return floatval(number_format((float)$value, 2, '.', ''));
-
-//         // return floatval(number_format($value, 2));
-//     }
     public function user() {
     	return $this->belongsTo('App\User');
     }
