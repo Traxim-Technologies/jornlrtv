33c51391c069025064cc6b1f0191b06072cf37eb
bhawyacodegama <bhawya@codegama.com>
2019-09-28 16:11:03 +0530

    Added Log

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -73,11 +73,8 @@ class CommonHelper {
 	                    Log::info("isValid FINAL STATUS - ".$isValid);
 
 	                }
-
-                    Log::info("Status :".print_r($isValid,true));
                     
 	                if($isValid) {
-                        Log::info("Email data :".print_r($email_data,true));
 
 	                    if (Mail::queue($page, ['email_data' => $email_data,'site_url' => $site_url], 
 	                            function ($message) use ($email, $subject) {
--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -231,7 +231,10 @@
 
                         }
 
+                        Log::info("Status :".print_r($isValid,true));
+                    
                         if($isValid) {
+                            Log::info("Email data :".print_r($email_data,true));
 
                             if (Mail::queue($page, array('email_data' => $email_data,'site_url' => $site_url), 
                                     function ($message) use ($email, $subject) {
