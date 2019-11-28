0ce5e58e26631b079e5cfc96be0f435b3fdf9425
bhawyacodegama <bhawya@codegama.com>
2019-11-28 11:04:00 +0530

    Issue Fixes

--- app/VideoTape.php
+++ app/VideoTape.php
@@ -294,12 +294,6 @@ class VideoTape extends Model
 
     }
 
-    public function getBellNotifications() {
-
-         return $this->hasMany('App\BellNotification', 'video_tape_id', 'id');
-
-    }
-
     public function toArray()
     {
         $array = parent::toArray();
@@ -414,16 +408,6 @@ class VideoTape extends Model
                     $model->getVideoAds->delete();   
 
                 }             
-
-            }
-
-            if (count($model->getBellNotifications) > 0) {
-
-                foreach ($model->getBellNotifications as $key => $value) {
-
-                   $value->delete();    
-
-                }            
 
             }
 
